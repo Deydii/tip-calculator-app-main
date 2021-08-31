0da48302e439da0e843d3267a380ecda6279a8fb
Audrey LOURI <audrey.louri@gmail.com>
2021-08-31 16:09:15 +0200

    remove div in tip's section

--- src/components/Splitter/Tip/index.js
+++ src/components/Splitter/Tip/index.js
@@ -8,8 +8,6 @@ const Tip = () => {
         <button className="tips__button tip">5%</button>
         <button className="tips__button tip">10%</button>
         <button className="tips__button tip">15%</button>
-      </div>
-      <div className="tips__buttons">
         <button className="tips__button tip">25%</button>
         <button className="tips__button tip">50%</button>
         <input className="tip tip--custom" type="number" placeholder="Custom" value="" />
