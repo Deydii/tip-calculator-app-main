c77a86e23543695995ccadebcbc922367c9da2eb
Audrey LOURI <audrey.louri@gmail.com>
2021-09-10 12:53:31 +0200

    modify bill test organization and add assertion

--- src/tests/bill/Bill.test.js
+++ src/tests/bill/Bill.test.js
@@ -2,65 +2,78 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Bill from '../../components/Splitter/Bill/index';
 
-test('It should render bill component', () => {
-  render(
-  <Bill 
-    billValue="" 
-    onChangeBillValue={jest.fn()} 
-  />);
+describe('Bill component', () => {
+
+  test('It should render bill component', () => {
+    render(
+      <Bill 
+        billValue="" 
+        onChangeBillValue={jest.fn()} 
+      />);
+  });
+
+  test('It should render bill input', () => {
+    render(
+      <Bill 
+        billValue="" 
+        onChangeBillValue={jest.fn()} 
+      />);
+    const inputEl = screen.getByLabelText("Bill");
+    expect(inputEl).toBeInTheDocument();
+  });
+
+  test('Input should have type number attribute', () => {
+    render(
+      <Bill 
+        billValue="" 
+        onChangeBillValue={jest.fn()} 
+      />);
+    const inputEl = screen.getByLabelText("Bill");
+    expect(inputEl).toHaveAttribute("type", "number");
+  });
+
+  test('Initial input value should be empty', () => {
+    render(
+      <Bill 
+        billValue="" 
+        onChangeBillValue={jest.fn()} 
+      />);
+    const inputEl = screen.getByLabelText("Bill");
+    expect(inputEl.value).toBe("");
+  });
+
+  test('Input value should be equal to billValue prop', () => {
+    render(
+      <Bill 
+        billValue="200" 
+        onChangeBillValue={jest.fn()} 
+      />);
+    const inputEl = screen.getByLabelText("Bill");
+    expect(inputEl.value).toBe("200");
+  });
+
+  test('onChange function should be called when user is typing in the input', () => {
+    const changeValue = jest.fn();
+    render(
+      <Bill 
+        billValue="" 
+        onChangeBillValue={changeValue} 
+      />);
+    const inputEl = screen.getByLabelText("Bill");
+
+    userEvent.type(inputEl, "200");
+    expect(changeValue).toHaveBeenCalled();
+  });
+
+  test('onChange function should not be called if input value is not a number', () => {
+    const changeValue = jest.fn();
+    render(
+      <Bill 
+        billValue="" 
+        onChangeBillValue={changeValue} 
+      />);
+    const inputEl = screen.getByLabelText("Bill");
+    userEvent.type(inputEl, "bill");
+    expect(changeValue).not.toHaveBeenCalled();
+  });
 });
-
-test('It should render bill input', () => {
-  render(
-  <Bill 
-    billValue="" 
-    onChangeBillValue={jest.fn()} 
-  />);
-  const inputEl = screen.getByLabelText('Bill');
-  expect(inputEl).toBeInTheDocument();
-});
-
-test('Input should have type number attribute', () => {
-  render(
-  <Bill 
-    billValue="" 
-    onChangeBillValue={jest.fn()} 
-  />);
-  const inputEl = screen.getByLabelText('Bill');
-  expect(inputEl).toHaveAttribute('type', 'number');
-});
-
-test('Input value should be empty', () => {
-  render(
-    <Bill 
-      billValue="" 
-      onChangeBillValue={jest.fn()} 
-    />);
-  const inputEl = screen.getByLabelText('Bill');
-  expect(inputEl.value).toBe('');
-});
-
-test('onChange function should be called', () => {
- const changeValue = jest.fn()
-  render(
-    <Bill 
-      billValue="" 
-      onChangeBillValue={changeValue} 
-    />);
-  const inputEl = screen.getByLabelText("Bill");
-
-  userEvent.type(inputEl, '200')
-  expect(changeValue).toHaveBeenCalled();
-});
-
-test('onChange function should not be called if input value is not a number', () => {
-  const changeValue = jest.fn();
-  render(
-    <Bill 
-      billValue="" 
-      onChangeBillValue={changeValue} 
-    />);
-  const inputEl = screen.getByLabelText("Bill");
-  userEvent.type(inputEl, 'bill')
-  expect(changeValue).not.toHaveBeenCalled();
-})
