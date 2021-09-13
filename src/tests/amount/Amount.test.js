08b2574a56b04d3ed85e35787a845d2a147188dc
Audrey LOURI <audrey.louri@gmail.com>
2021-09-13 22:45:29 +0200

    finish amount component test

--- src/tests/amount/Amount.test.js
+++ src/tests/amount/Amount.test.js
@@ -41,6 +41,37 @@ describe('Amount component', () => {
       expect(buttonEl).not.toBeDisabled();
     });
 
+    test('tipAmount value and total value should be a number', () => {
+      const tipAmountValue = 10;
+      const totalValue = 76.67;
+      render(
+        <Amount 
+          tipAmount={tipAmountValue}
+          total={totalValue}
+          isDisabled={false}
+          onClickResetButton={jest.fn()}
+        />
+      );
+      expect(typeof tipAmountValue).toBe("number");
+      expect(typeof totalValue).toBe("number");
+    });
+
+    test('tipAmount value should have 2 decimales', () => {
+      const tipAmountValue = 10.233;
+      render(
+        <Amount 
+          tipAmount={tipAmountValue}
+          total={76.67}
+          isDisabled={false}
+          onClickResetButton={jest.fn()}
+        />
+      );
+      const pEl = screen.getByTestId("amount__price");
+      const pElText = parseFloat(pEl.textContent.slice(1));
+      expect(tipAmountValue).not.toEqual(pElText);
+    });
+    
+
     test('onClick function should be called when user clicks on reset button', () => {
       const onClickButton = jest.fn();
       render(
@@ -54,5 +85,5 @@ describe('Amount component', () => {
       const buttonEl = screen.getByRole("button");
       userEvent.click(buttonEl);
       expect(onClickButton).toHaveBeenCalled();
-    })
-})
+    });
+});
